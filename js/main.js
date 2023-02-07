ba5eebf75621ec62785f5a10885c6a08d762cbee
marcin <brand.marcin@gmail.com>
2023-02-07 16:52:45 +0000

    zadanie43669

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
 let num1 = 2;
 let num2 = 5;
 
-function bn(num1, num2) {
-    return Math.max;
+// function bn(num1, num2) {
+//     return Math.max;
 
+// }
+
+// console.log(Math.max(2, 5));
+
+function bn(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
 }
 
-console.log(Math.max(2, 5));
+console.log(bn(num1, num2));
